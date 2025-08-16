ce6c05ad65149c69ef9ee7375f154d629524a715
Tientcheu Israel <tientcheuisrael@gmail.com>
2025-08-16 20:53:42 +0100

    Test values with input and output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashSceen } from './components/splash-sceen/splash-sceen';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,SplashSceen],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('cefti-website');
+
+  noms = ['Banane']
+
+  recevoir(nom:string){
+    this.noms.push(nom)
+  }
 }
