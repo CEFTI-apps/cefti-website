925bc9fc67e86f17878d6f9a4a959c7a0ac145fd
TilStack <israel01tientcheu@gmail>
2025-08-18 15:25:06 +0100

    Implement layout structure with public and empty layouts; add routing and environment configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/ui/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Public layout routes
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path : 'home',
+        component: Home,
+        data : { layout : 'public' }
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,66 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { distinctUntilChanged, filter, map, Observable, startWith, Subject, takeUntil } from 'rxjs';
+import { PublicLayout } from "./layouts/public-layout/public-layout";
+import { EmptyLayout } from "./layouts/empty-layout/empty-layout";
+import { CommonModule } from '@angular/common';
+import { isBrowser } from './core/utils/is-broswer';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PublicLayout, EmptyLayout,PublicLayout,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   protected readonly title = signal('cefti-website');
+   protected readonly router = inject(Router)
+   protected readonly activatedRoute =  inject(ActivatedRoute)
+  private readonly destroy$ = new Subject<void>();
 
-  compteur = signal(0);
+   /** Layout courant selon la route active */
+   protected readonly currentLayout$: Observable<    'public' | 'empty'> = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    startWith(null),
+    map(() => {
+      let route = this.activatedRoute.firstChild;
+      while (route?.firstChild) {
+        route = route.firstChild;
+      }
+      return (
+        (route?.snapshot.data?.['layout'] as
+          | 'public'
+          | 'empty') || 'public'
+      );
+    }),
+    distinctUntilChanged()
+   )
+
+
+  protected resetPosition() {
+    if (isBrowser()) {
+      window.scrollTo({ top: 0, behavior: 'auto' });
+    }
+  }
+
+  ngOnInit(): void {
+      this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => {
+        
+        setTimeout(() => {
+          if (isBrowser()) {
+            window.scrollTo({ top: 0, behavior: 'auto' });
+          }
+        }, 0);
+      });
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 }
