15f73ab3af68eedb80f56ce39b3912c2ab9a119d
TilStack <israel01tientcheu@gmail>
2025-08-21 17:47:44 +0100

    Refactor routing and components for program, activity, employees, contact, visit, and request

---
+++ src/app/features/ui/activity-school/activity-school.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-school',
+  imports: [],
+  templateUrl: './activity-school.html',
+  styleUrl: './activity-school.scss'
+})
+export class ActivitySchool {
+
+}
---
+++ src/app/features/ui/programmes/programmes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programmes',
+  imports: [],
+  templateUrl: './programmes.html',
+  styleUrl: './programmes.scss'
+})
+export class Programmes {
+
+}
